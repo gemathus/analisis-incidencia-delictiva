9dd067a03c2d154f1fe057bf104ffb266b1b3c9f
Gerardo Mathus <gerardo@nextia.mx>
2019-07-09 17:10:49 -0500

    finishs script 1

--- src/1_summarize_dataset.py
+++ src/1_summarize_dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 import dateutil
 from pprint import pprint
 meses = {"Enero":1, "Febrero":2, "Marzo":3, "Abril":4, "Mayo":5, "Junio":6, "Julio":7, "Agosto":8, "Septiembre":9, "Octubre":10,"Noviembre":11, "Diciembre":12, }
-
+alcaldias_cdmx = ['CUAUHTEMOC', 'IZTAPALAPA', 'GUSTAVO A MADERO', 'BENITO JUAREZ', 'COYOACAN', 'MIGUEL HIDALGO', 'ALVARO OBREGON', 'VENUSTIANO CARRANZA', 'TLALPAN', 'AZCAPOTZALCO','IZTACALCO','XOCHIMILCO','TLAHUAC', 'LA MAGDALENA CONTRERAS', 'CUAJIMALPA DE MORELOS', 'MILPA ALTA']
 def main():
     print("Leyendo datos de CSV y convirtinedo a Pandas.DataFrame...")
     carpetas_investigacion = pd.read_csv('../data/carpetas-de-investigacion-pgj-cdmx.csv', delimiter=';')
@@ -15,7 +15,7 @@ def main():
 
     print("Agregando columna de mes numérico")
     carpetas_investigacion['mes_num'] = carpetas_investigacion['mes_hechos'].map(lambda x: meses[x])
-    valores_2016_a_2018 = carpetas_investigacion[carpetas_investigacion['año_hechos'] < 2019]
+    #valores_2016_a_2018 = carpetas_investigacion[carpetas_investigacion['año_hechos'] < 2019]
     print("Quitando columnas no usadas")
     carpetas_investigacion = carpetas_investigacion.drop(['fiscalía', 
                             'agencia', 
@@ -30,11 +30,15 @@ def main():
 
     pprint(carpetas_investigacion.head())
     input("continuar...")
+    print("Quitando datos que no sean de la CDMX")
+    carpetas_investigacion = carpetas_investigacion[carpetas_investigacion['alcaldia_hechos'].isin(alcaldias_cdmx)]
+    pprint(carpetas_investigacion['alcaldia_hechos'].value_counts())
+    input("continuar...")
     print("guardando nuevo dataset")
     carpetas_investigacion.to_csv('../data/carpetas_2016_2019.csv', sep=";")
     print("dataset cuardado en '../data/carpetas_2016_2019.csv'")
     input("Presiona cualquier tecla para terminar")
     print("Bye")
     
-def if __name__ == "__main__":
+if __name__ == "__main__":
     main()
